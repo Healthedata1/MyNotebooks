fc580137d4df4b76ea926d43b75f4e4fabd1d80d
Eric Haas <ehaas@healthedatainc.com>
2019-06-19 08:12:18 -0700

    update IG-maker

---
+++ IG-maker/ig_pages_template.py
@@ -0,0 +1,55 @@
+pages = {
+	"grouping": [
+		{
+			"name": "base"
+		}
+	],
+	"page": {
+		"nameUrl": "index.html",
+		"title": "Home",
+		"generation": "markdown",
+		"page": [
+			{
+				"nameUrl": "guidance.html",
+				"title": "General Guidance",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "profiles.html",
+				"title": "Profiles and Extensions",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "operations.html",
+				"title": "Operations",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "terminology.html",
+				"title": "Terminology",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "searchparameters.html",
+				"title": "Search Parameters",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "capstatements.html",
+				"title": "Capability Statements",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "security.html",
+				"title": "Security",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "downloads.html",
+				"title": "Downloads",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "all-examples.html",
+				"title": "All Examples",
+				"generation": "markdown"
+			}, {
+				"nameUrl": "toc.html",
+				"title": "Table of Contents",
+				"generation": "html"
+			}
+		]
+	}
+}
